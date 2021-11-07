91def10d0d02d86bbc396b37f3b18a517dc83dfb
Habibur Rahman <hbrawnak@gmail.com>
2021-11-07 17:17:29 +0600

    Person and Page post create api

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\ProcessPost;
+use App\Models\Post;
+use App\Repository\PageRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function user_post(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'text' => 'required|string|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Data validation error',
+                'response' => $validator->errors()
+            ], 401);
+        }
+
+
+        $user    = Auth::user();
+        $process = new ProcessPost(
+            $user->uuid, Post::SOURCE_TYPE_USER, $request->input('text')
+        );
+        $this->dispatch($process);
+
+        return response()->json([
+            'error' => false,
+            'message' => 'Post created',
+            'response' => []
+        ], 201);
+    }
+
+    public function page_post(Request $request, $pageId, PageRepository $pageRepository)
+    {
+        $validator = Validator::make($request->all(), [
+            'text' => 'required|string|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Data validation error',
+                'response' => $validator->errors()
+            ], 401);
+        }
+
+        if (!$pageRepository->exist($pageId)) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Data validation error',
+                'response' => 'Following ID doesn\'t exist'
+            ], 401);
+        }
+
+        $user    = Auth::user();
+
+        if (!$pageRepository->owner($pageId, $user->uuid)) {
+            return response()->json([
+                'error' => true,
+                'message' => 'Data validation error',
+                'response' => 'The page doesn\'t belong to you'
+            ], 401);
+        }
+
+        $process = new ProcessPost(
+            $pageId, Post::SOURCE_TYPE_PAGE, $request->input('text')
+        );
+        $this->dispatch($process);
+
+        return response()->json([
+            'error' => false,
+            'message' => 'Post created',
+            'response' => []
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -18,6 +19,7 @@ Route::prefix('auth')->group(function () {
 Route::prefix('page')->group(function () {
     Route::group(['middleware' => ['jwt.verify']], function () {
         Route::post('/create', [PageController::class, 'create']);
+        Route::post('/{pageId}/attach-post', [PostController::class, 'page_post']);
     });
 });
 
@@ -25,5 +27,12 @@ Route::prefix('follow')->group(function () {
     Route::group(['middleware' => ['jwt.verify']], function () {
         Route::get('/person/{personId}', [FollowerController::class, 'follow_person']);
         Route::get('/page/{pageId}', [FollowerController::class, 'follow_page']);
+    });
+});
+
+Route::prefix('person')->group(function () {
+    Route::group(['middleware' => ['jwt.verify']], function () {
+        Route::post('/attach-post', [PostController::class, 'user_post']);
+        //Route::get('/feed', []);
     });
 });
